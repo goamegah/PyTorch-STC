f8b1d8c46475cc6c6a8ffed0b883393f8d3c2df8
Godwin AMEGAH <komlan.godwin.amegah@gmail.com>
2024-03-14 00:32:19 +0100

    adapt metric to recent version of packages

--- torchclust/metrics/metrics.py
+++ torchclust/metrics/metrics.py
@@ -1,18 +1,29 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
-from sklearn.metrics import normalized_mutual_info_score
+from scipy.optimize import linear_sum_assignment
+from sklearn.metrics.cluster import normalized_mutual_info_score
 
+ # Calculer le NMI
 nmi = normalized_mutual_info_score
 
-
 def acc(y_true, y_pred):
-    y_true = y_true.astype(np.int64)
-    assert y_pred.size == y_true.size
-    D = max(y_pred.max(), y_true.max()) + 1
-    w = np.zeros((D, D), dtype=np.int64)
-    for i in range(y_pred.size):
-        w[y_pred[i], y_true[i]] += 1
-    from sklearn.utils.linear_assignment_ import linear_assignment
-    ind = linear_assignment(w.max() - w)
-    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
+    # Vérifier que les deux tableaux ont la même taille
+    assert len(y_pred) == len(y_true), "Les tableaux y_pred et y_true doivent avoir la même taille."
+
+    # Nombre de classes
+    num_classes = max(max(y_pred), max(y_true)) + 1
+
+    # Initialiser la matrice de confusion
+    confusion_matrix = np.zeros((num_classes, num_classes), dtype=np.int64)
+
+    # Remplir la matrice de confusion
+    for true_label, pred_label in zip(y_true, y_pred):
+        confusion_matrix[pred_label, true_label] += 1
+
+    # Trouver la correspondance optimale entre les étiquettes prédites et les étiquettes réelles
+    row_ind, col_ind = linear_sum_assignment(confusion_matrix.max() - confusion_matrix)
+
+    # Calculer la précision en fonction de la correspondance optimale
+    total_correct = sum([confusion_matrix[row, col] for row, col in zip(row_ind, col_ind)])
+    accuracy = total_correct / len(y_pred)
+
+    return accuracy
