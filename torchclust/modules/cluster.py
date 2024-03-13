b8c5d0ad59aba31616c9608b58d8eb4b86f2f66f
Godwin AMEGAH <komlan.godwin.amegah@gmail.com>
2024-03-14 00:33:17 +0100

    readapt cluster algorithm layers

--- torchclust/modules/cluster.py
+++ torchclust/modules/cluster.py
@@ -8,8 +8,23 @@ class ClusteringLayer(nn.Module):
         super(ClusteringLayer, self).__init__()
         self.clusters = nn.Parameter(torch.randn(n_clusters, input_dim))
 
-    def forward(self, x):
+    def init_clusters(self, x: torch.Tensor):
+        self.clusters.data = x
+    
+    def solf_clustering(self, x: torch.Tensor):
+        assert len(x.shape) == 2, "Input tensor must be 2D." 
+
         q = 1.0 / (1.0 + (torch.sum((x.unsqueeze(1) - self.clusters) ** 2, dim=2) / 1.0))
         q = q ** 2 / q.sum(0)
         q = (q.T / q.sum(1)).T
         return q
+
+    def target_distribution(self, q : torch.Tensor):
+        p = q ** 2 / q.sum(0)
+        p = (p.T / p.sum(1)).T
+        return p
+
+    def forward(self, x : torch.Tensor):
+        q = self.solf_clustering(x)
+        p = self.target_distribution(q)
+        return q, p
